43e0bd4f8d2d529ad2106f31808868dc3c9018db
withersc <withersc@bhmam.ac.uk>
2016-05-21 00:27:53 +0100

    set up gulp task runner

--- src/Alis.Site.Angular/gulpfile.js
+++ src/Alis.Site.Angular/gulpfile.js
@@ -1,22 +1,26 @@
-﻿/// <binding Clean='clean' />
+﻿/// <binding BeforeBuild='min, min:css, min:js' AfterBuild='inject' Clean='clean' />
 
 var gulp = require("gulp"),
     rimraf = require("rimraf"),
     concat = require("gulp-concat"),
     cssmin = require("gulp-cssmin"),
     uglify = require("gulp-uglify"),
+    inject = require("gulp-inject"),
     project = require("./project.json");
 
 var paths = {
-    webroot: "./" + project.webroot + "/"
+    webroot: "./" + project.webroot + "/",
+    resources: "./" + project.siteResources + "/"
 };
 
-paths.js = paths.webroot + "js/**/*.js";
-paths.minJs = paths.webroot + "js/**/*.min.js";
-paths.css = paths.webroot + "css/**/*.css";
-paths.minCss = paths.webroot + "css/**/*.min.css";
-paths.concatJsDest = paths.webroot + "js/site.min.js";
-paths.concatCssDest = paths.webroot + "css/site.min.css";
+paths.jsroot = paths.resources + "js/**/*.js";
+paths.minJs = paths.resources + "js/**/*.min.js";
+paths.css = paths.resources + "css/**/*.css";
+paths.minCss = paths.resources + "css/**/*.min.css";
+paths.concatJs = "js/site.min.js";
+paths.concatJsDest = paths.webroot + paths.concatJs;//call dest full or some such?
+paths.concatCss = "css/site.min.css";
+paths.concatCssDest = paths.webroot + paths.concatCss;
 
 gulp.task("clean:js", function (cb) {
     rimraf(paths.concatJsDest, cb);
@@ -29,7 +33,7 @@ gulp.task("clean:css", function (cb) {
 gulp.task("clean", ["clean:js", "clean:css"]);
 
 gulp.task("min:js", function () {
-    gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+    gulp.src([paths.jsroot, "!" + paths.minJs])
         .pipe(concat(paths.concatJsDest))
         .pipe(uglify())
         .pipe(gulp.dest("."));
@@ -43,3 +47,20 @@ gulp.task("min:css", function () {
 });
 
 gulp.task("min", ["min:js", "min:css"]);
+
+gulp.task("inject", function () {
+    gulp.src("./Views/Shared/_Layout.cshtml")
+        .pipe(inject(gulp.src(paths.concatJsDest, { read: false }), {
+            transform: function(filepath) {
+               // if (filepath.slice(-5) === '.docx') {
+                return ' <script src="' + filepath.substring(paths.webroot.length - 2) + '"></script>';
+                //}
+                // Use the default transform as fallback: 
+                return inject.transform.apply(inject.transform, arguments);
+            }
+        }))
+    .pipe(gulp.dest("./Views/Shared/"));;
+    // It's not necessary to read the files (will speed up things), we're only after their paths: 
+
+      
+});
