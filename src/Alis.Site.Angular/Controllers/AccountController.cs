1c5cb33b7fb46790922825c0a879a29c060dd0c6
Chris <withersc@adf.bham.ac.uk>
2016-06-28 15:52:54 +0100

    Started trying to sort out auth

---
+++ src/Alis.Site.Angular/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Alis.Site.Angular.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Login()
+        {
+            var client = new ServiceStack.JsonServiceClient("https://alis.ws");
+
+
+
+            return View();
+        }
+    }
+}
